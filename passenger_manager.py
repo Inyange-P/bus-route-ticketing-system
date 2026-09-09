de11e1cdb7cf16361b900d472624d4fd82cc5946
Chinelo <c.nnamdika@alustudent.com>
2026-09-09 18:59:52 +0400

    feat: implement PassengerManager

---
+++ passenger_manager.py
@@ -0,0 +1,28 @@
+from passenger import Passenger
+
+class PassengerManager:
+    # Manages all passengers in the system.
+
+    def __init__(self):
+        self.passengers = {}
+
+    def generate_passenger_id(self):
+        # if there are no passengers yet start from ID 1.
+        if not self.passengers:
+            return 1
+        # otherwise, continue from the highest existing passenger ID.
+        return max(self.passengers.keys()) + 1
+
+    def register_passenger(self, name, phone):
+        passenger_id = self.generate_passenger_id()
+        passenger = Passenger(passenger_id, name, phone)
+        self.passengers[passenger_id] = passenger
+        return passenger
+
+    def display_passengers(self):
+        if not self.passengers:
+            print("No passengers registered.")
+            return
+        print("\n----- REGISTERED PASSENGERS -----")
+        for passenger in self.passengers.values():
+            print(passenger)
